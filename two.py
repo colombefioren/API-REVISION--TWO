91644f6255a8a131a87e5806fb9d3ac94d4018b5
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-30 21:13:08 +0300

    feat: GET/ events by their id

--- two.py
+++ two.py
@@ -1,6 +1,8 @@
 import json
+from typing import List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import Response
 
@@ -16,3 +18,24 @@ def get_version(request : Request):
     if accept_type != "application/json":
         return Response(content=json.dumps({"message": "Unrequired media type!"}),status_code=406,media_type="application/json")
     return Response(content=json.dumps({"version":"1.0.0"}),status_code=200,media_type="application/json")
+
+class Event(BaseModel):
+    id : int
+    name : str
+    start_date : str
+    end_date : str
+
+events_store : List[Event] = []
+
+def serialized_events_store():
+    converted_event = []
+    for event in events_store:
+        converted_event.append(event.model_dump())
+    return converted_event
+
+@app.get("/events/{events_id}")
+def get_event(events_id : int):
+    for event in events_store:
+        if event.id == events_id:
+            return Response(content=json.dumps(event.model_dump()),status_code=200,media_type="application/json")
+    return Response(content=json.dumps({"message":f"Event with id {events_id} not found!"}),status_code=404,media_type="application/json")
