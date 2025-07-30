54551aad70e1f72bbfdfc2e9d6b04d4184f72b26
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-30 20:21:19 +0300

    feat: GET/ endpoint

--- one.py
+++ one.py
@@ -1,11 +1,20 @@
 import json
 
 from fastapi import FastAPI
+from starlette.requests import Request
 from starlette.responses import Response
 
-app = FastAPI();
+app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return Response(content=json.dumps({"message" : "Hello World!"}),status_code=200,media_type="application/json")
+def read_root(request : Request,name: str = "Non défini(e)",is_teacher : bool = False):
+    accept_type = request.headers.get("accept")
+    if accept_type != "text/plain" and accept_type != "text/html":
+        return Response(content=json.dumps({"message":"Media type not supported!"}),status_code=400)
+    if is_teacher:
+         return Response(content=json.dumps({"message":f"Hello teacher {name}!"}),status_code=200)
+    if name != "Non défini(e)":
+        return Response(content=json.dumps({"message":f"Hello {name}!"}),status_code=200)
+    return Response(content=json.dumps({"message":f"Hello World!"}),status_code=200)
+
 
