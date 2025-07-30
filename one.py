7482d9f25abb80d17ab7e5979006ce843c7fc33d
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-30 20:26:02 +0300

    feat: GET/top-secret

--- one.py
+++ one.py
@@ -17,4 +17,11 @@ def read_root(request : Request,name: str = "Non défini(e)",is_teacher : bool =
         return Response(content=json.dumps({"message":f"Hello {name}!"}),status_code=200)
     return Response(content=json.dumps({"message":f"Hello World!"}),status_code=200)
 
+@app.get("/top-secret")
+def read_top_secret(request : Request):
+    key_value = request.headers.get("Authorization")
+    if key_value is None or key_value != "my-secret-key":
+        return Response(content=json.dumps({"message":f"{key_value} is not the secret key!"}),status_code=403)
+    return Response(content=json.dumps({"message":"Hello Top Secret!"}),status_code=200)
+
 
